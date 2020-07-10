98924f110bdca3430b220df449530b5d5e03959a
lethienhongson96 <lethienhongson96@gmail.com>
2020-07-10 08:31:05 +0700

    -u

---
+++ AccessModifier/QuadraticEquation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModifier
+{
+    class QuadraticEquation
+    {
+        private double _A, _B, _C;
+
+        public QuadraticEquation(double a,double b,double c)
+        {
+            this._A = a;
+            this._B = b;
+            this._C = c;
+        }
+
+        public double A() => this._A;
+        public double B() => this._B;
+        public double C() => this._C;
+
+        public double GetDiscriminant() => (this._B * this._B) - (4 * this._A * this._C);
+
+        public double GetRoot1()=> (-this._B + Math.Sqrt(this.GetDiscriminant())) / (2 * this._A);
+
+        public double GetRoot2() => (-this._B - Math.Sqrt(this.GetDiscriminant())) / (2 * this._A);
+
+        public string GetResult()
+        {
+            if (this.GetDiscriminant() < 0)
+            {
+                return "The equation has no roots";
+            }
+
+            else if (this.GetDiscriminant()==0)
+            {
+                return $"The equation have one root is: {this.GetRoot1()}";
+            }
+
+            return $"The equation has two roots: {this.GetRoot1()} and {this.GetRoot2()}";
+        }
+
+        public static void Main()
+        {
+            QuadraticEquation pt1 = new QuadraticEquation(1,-8,16);
+            Console.WriteLine(pt1.GetResult());
+        }
+    }
+}
---
+++ Inheritance/TestShape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class TestShape
+    {
+        public static void Main()
+        {
+            Random rnd = new Random();
+            int percent = rnd.Next(1,100);
+            Square square = new Square();
+            square.Resize(percent);
+            Console.WriteLine(square.GetSide());
+        }
+    }
+}
